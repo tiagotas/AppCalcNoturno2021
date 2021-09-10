613ad25e3f7882b251263696a52751eff7097604
TIAGO SILVA <frost.tiago@gmail.com>
2021-09-09 22:24:59 -0300

    Adição de comportamento dos botoes

--- AppCalcNoturno2021/AppCalcNoturno2021/MainPage.xaml.cs
+++ AppCalcNoturno2021/AppCalcNoturno2021/MainPage.xaml.cs
@@ -14,5 +14,52 @@ namespace AppCalcNoturno2021
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked_7(object sender, EventArgs e)
+        {
+            visor.Text = visor.Text + "7";
+        }
+
+        private void Button_Clicked_8(object sender, EventArgs e)
+        {
+            visor.Text += "8";
+        }
+
+        private void Button_Clicked_9(object sender, EventArgs e)
+        {
+            visor.Text += "9";
+        }
+
+        private void Button_Clicked_4(object sender, EventArgs e)
+        {
+            visor.Text += "4";
+        }
+
+        private void Button_Clicked_5(object sender, EventArgs e)
+        {
+            visor.Text += "5";
+        }
+
+        private void Button_Clicked_6(object sender, EventArgs e)
+        {
+            visor.Text += "6";
+        }
+
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+            visor.Text += "3";
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            visor.Text += "1";
+
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            visor.Text += "2";
+
+        }
     }
 }
